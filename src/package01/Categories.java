2b39c254ed96e7095cc23950d508128d0d4c915b
Morrobel07 <morrobelc03@gmail.com>
2025-09-06 14:40:29 -0400

    Hoy

---
+++ src/package01/Categories.java
@@ -0,0 +1,49 @@
+package package01;
+
+public class Categories {
+
+    private int categoryID;
+    private String categoryName;
+    private String description;
+    private String picture;
+
+    public Categories(int categoryID, String categoryName, String description, String picture) {
+        this.categoryID = categoryID;
+        this.categoryName = categoryName;
+        this.description = description;
+        this.picture = picture;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+}
