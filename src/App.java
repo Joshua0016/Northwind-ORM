e02ae4adcdc4579fd71910db2642e8067c817c21
yamatos <58537167+yimyaneuris@users.noreply.github.com>
2025-09-06 17:13:47 -0400

    Update App.java

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ import package01.Product;
 
 public class App {
 
+    // favor generar un menu para las opcines del sistema a entregar, mientras menos iteracion tenemos con el usriao mejor para nosotros
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
